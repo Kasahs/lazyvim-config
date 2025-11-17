8b078f7c145cdf8ffcff3238060da3ab2dd088d9
Shasak Raina <shasak.sync@gmail.com>
2025-11-17 14:56:16 +0530

    added toggle for blink.cmp

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,13 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+--
+local function toggle_blink_cmp()
+  local blink = require("blink.cmp")
+  vim.b.blink_enabled = not vim.b.blink_enabled
+  if not vim.b.blink_enabled then
+    blink.hide() -- Hide any open menu immediately
+  end
+  vim.notify("blink.cmp " .. (vim.b.blink_enabled and "enabled" or "disabled"))
+end
+vim.keymap.set({ "n", "i" }, "<leader>tc", toggle_blink_cmp, { desc = "Toggle blink.cmp" })
